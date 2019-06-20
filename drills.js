970f3321544f66422fd363a8b427bede42da95cd
David Nordeen <davidnordeen@gmail.com>
2019-06-20 10:22:28 -0700

    1

--- drills.js
+++ drills.js
@@ -1,1 +1,47 @@
-hello
+'use strict';
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+  // console.log(mergeSort(left), 'left');
+  left = mergeSort(left);
+  // console.log(mergeSort(right), 'right');
+  right = mergeSort(right);
+  console.log(merge(left, right, array), 'merge');
+  return merge(left, right, array);
+}
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+}
+
+mergeSort([21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]);
+
+//1
+//a. 1,21
+//b. half of the list would be sorted
+//c.21 and 1
+//d. 1, 21 and 26,45
